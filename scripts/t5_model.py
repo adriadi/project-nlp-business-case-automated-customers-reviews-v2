46a4f4c86d6f9eb1f869f3e2e5d279a4641d488a
Julien <julien.sebag@me.com>
2025-06-11 17:15:54 +0200

    Remove T5 Tokenizer

--- scripts/t5_model.py
+++ scripts/t5_model.py
@@ -1,9 +1,4 @@
-from transformers import T5ForConditionalGeneration, T5Tokenizer
 from collections import Counter
-
-model_name = "t5-small"
-tokenizer = T5Tokenizer.from_pretrained(model_name)
-model = T5ForConditionalGeneration.from_pretrained(model_name)
 
 
 def build_summary_prompt(text):
