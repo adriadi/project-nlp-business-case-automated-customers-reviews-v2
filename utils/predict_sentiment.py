7b9c815efadeae110a24b8baff02264e29f745a2
adrianna <adrianna.dziadyk@gmail.com>
2025-06-13 11:33:19 +0200

    cleaned sentimental classification and moved function to utils

---
+++ utils/predict_sentiment.py
@@ -0,0 +1,8 @@
+#Function to predict sentiment
+def classify_sentiment(text):
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, max_length=512).to(device)
+    with torch.no_grad():
+        outputs = model(**inputs)
+        probs = torch.nn.functional.softmax(outputs.logits, dim=1)
+        predicted_class = torch.argmax(probs).item()
+    return labels[predicted_class]
